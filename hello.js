0a9c30155326952014175876d7b7609656fdbb40
Johannes Kümmel <jkymmel@gmail.com>
2017-01-25 20:33:42 +0200

    added basic get to hello

--- hello.js
+++ hello.js
@@ -2,3 +2,12 @@ var app = require('express')();
 var http = require('http').Server(app);
 
 console.log("helo m8");
+
+app.get("/", function(req, res) {
+   res.send("yello");
+    console.log("someone hacked us!!!!")
+});
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
